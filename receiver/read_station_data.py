61fda78f43ee78c23f3905f052c5ce4886852b98
Pedro-vd20 <pedro209.vd@gmail.com>
2022-07-21 17:35:25 +0400

    bug fixes, new id-format, added read_data script

---
+++ receiver/read_station_data.py
@@ -0,0 +1,153 @@
+#from pylab import *
+import json
+import os
+from datetime import datetime
+from dateutil.parser import parse as timestring_to_datetime
+import numpy as np
+import pandas as pd
+
+# collect data in numpy format
+def collect_data(station_num):
+    path = '/home/pv850/received_files/station' + str(station_num) + '/'
+    try:
+        files = os.listdir(path)
+    except FileNotFoundError as e:
+        print('Station not found, please pass station_num parameter as integer corresponding to station')
+        print(path, 'not found')
+        raise(e)
+
+    # remove checksum files
+    files = [f if 'sha256' not in f else None for f in files]
+    while None in files:
+        files.remove(None)
+
+    files.sort()
+
+    # find out how many files left
+    num_files = len(files)
+    
+    # find out number of sensors for each
+    with open(path + files[0], 'r') as f_read:
+        data = json.load(f_read)
+    num_pm = len(data['particulate_matter'])
+    num_air_sens = len(data['air_sensor'])
+    
+    ##########
+    # initialize all data
+    pm = {'PM1count': np.zeros((num_pm, num_files), dtype=float),
+          'PM1mass': np.zeros((num_pm, num_files), dtype=float),
+          'PM2.5count': np.zeros((num_pm, num_files), dtype=float),
+          'PM2.5mass': np.zeros((num_pm, num_files), dtype=float),
+          'PM10count': np.zeros((num_pm, num_files), dtype=float),
+          'PM10mass': np.zeros((num_pm, num_files), dtype=float),
+          # the following only appear in either the sps30 or NEXTPM, but not both, so must 
+          # check if key is present
+          'sensor_T': np.zeros((num_pm, num_files), dtype=float),
+          'sensor_RH': np.zeros((num_pm, num_files), dtype=float),
+          'PM4count': np.zeros((num_pm, num_files), dtype=float),
+          'PM4mass': np.zeros((num_pm, num_files), dtype=float),
+          'PM0.5count': np.zeros((num_pm, num_files), dtype=float),
+          'PM0.5mass': np.zeros((num_pm, num_files), dtype=float),
+          'Typical_particle_size': np.zeros((num_pm, num_files), dtype=float)
+         }
+    air_sens = {'humidity': np.zeros((num_air_sens, num_files), float),
+                'temperature': np.zeros((num_air_sens, num_files), float),
+                'pressure': np.zeros((num_air_sens, num_files), float)
+               }
+
+    gps = {'time': np.zeros(num_files, dtype=datetime),
+           'latitude': np.zeros(num_files, dtype=float), 
+           'lat_dir': np.empty(num_files, dtype=str),
+           'longitude': np.zeros(num_files, dtype=float),
+           'lon_dir': np.empty(num_files, dtype=str),
+           'altitude': np.zeros(num_files, dtype=float),
+           'alt_unit': np.empty(num_files, dtype=str),
+           'num_sats': np.zeros(num_files, dtype=float),
+           'PDOP': np.zeros(num_files, dtype=float),
+           'HDOP': np.zeros(num_files, dtype=float),
+           'VDOP': np.zeros(num_files, dtype=float)
+          }
+    
+    
+    # loop through files collecting data
+    for i in range(len(files)):
+        # collect json data
+        with open(path + files[i], 'r') as f_read:
+            data = json.load(f_read)
+
+        # add pm
+        for sens in data['particulate_matter']:
+            index = int(sens['sensor'][-1]) # collect the index
+            # the assumption here is that no box will have more than 10 sensors
+            
+            for key in sens.keys():
+                if key in ('diagnostics', 'sensor'):
+                    continue
+                pm[key][index][i] = sens[key]
+ 
+        # add air sensor info
+        for sens in data['air_sensor']:
+            index = int(sens['sensor'][-1])
+
+            for key in sens.keys():
+                if key in ('type', 'sensor'):
+                    continue
+                air_sens[key][index][i] = sens[key]
+
+        # add gps info
+        for key in data['date_time_position'].keys():
+            if key in ('date', 'time'):
+                continue
+            temp = data['date_time_position'][key]
+            if temp == None:
+                gps[key][i] = np.nan
+            else:
+                gps[key][i] = temp
+        # add date time
+        gps['time'][i] = timestring_to_datetime(data['date_time_position']['date'] \
+                + 'T' + data['date_time_position']['time'] + 'Z')
+
+    return pm, air_sens, gps
+
+
+'''
+From:
+old_dict:   {
+                property: [ [sensor0_measure0, sensor0_measure1...],
+                            [sensor1_measure0, sensor1_measure1...],
+                            ...
+                          ]
+            }
+to:
+new_dict:   {
+                property_sensor0: [measure0, measure1,...],
+                property_sensor1: [measure0, measure1,...]
+'''
+def __unify_dict(new_dict, old_dict, sensor, num_sensors):
+    for key in old_dict.keys():
+        new_key = sensor + '_' + key
+        if num_sensors > 1:
+            for i in range(num_sensors):
+                new_dict[new_key + '_' + str(i)] = old_dict[key][i]
+        else:
+            new_dict[new_key] = old_dict[key]
+
+# collect data in pandas format
+def collect_data_pd(station_num):
+    pm, air_sens, gps = collect_data(station_num)
+    
+    # prepare dictionary for pandas
+    main_dict = {}
+    
+    # find how many sensors
+    num_pm = pm['PM1count'].shape[0] # both 'PM1count' and 'temperature' are 
+    num_air = air_sens['temperature'].shape[0] # measures present in all sensor types,
+                                        # regardless of manufacturer
+
+    __unify_dict(main_dict, pm, 'pm', num_pm)
+    __unify_dict(main_dict, air_sens, 'air_sensor', num_air)
+    __unify_dict(main_dict, gps, 'gps', 1)
+
+    return pd.DataFrame(main_dict)
+
+
--- receiver/receiver.py
+++ receiver/receiver.py
@@ -1,23 +1,273 @@
-from flask import Flask, request
+"""
+From https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
+with modifications.
+
+One can upload a file with curl using the command line:
+
+curl -F 'sensor_data_file=@<filename.txt>' -F 'checksum=@<chechsumfilename.sha256>' <web page address>
+
+where: 
+<filename> is the name of the file to be uploaded.
+<chechsumfilename> is the name of the textfile produced by running sha256sum 
+                   in binary mode on <filename.txt> 
+                   (e.g.: sha256sum -b pippo.txt > pippo.sha256).
+<web page address> is the url of the upload page, e.g. http://127.0.0.1:5000/
+"""
+
+import os
+import hashlib
+import datetime
+from flask import Flask, flash, request, redirect, url_for
+from numpy import void
+from werkzeug.utils import secure_filename
+from random import choice
+from string import ascii_letters
+import json
+
+# only accept txt and sha256 files
+ALLOWED_EXTENSIONS = {'txt', 'json', 'csv'}
+# place to permanently store files
+STORAGE_FOLDER = '/home/pv850/received_files/'
+DIAGNOSTICS = '/home/pv850/diagnostics/'
+# place where all pi ids are stored
+PI_ID_F = 'ids.json'
+# how long urls will be
+URL_LEN = 20
+
+#---------------------------------------------------------
+
+# write into log file
+def log(msg : str) -> void:
+    f = open('logs.txt', 'a')
+
+    # get current time
+    now = datetime.datetime.now()
+    f.write(now.strftime('[%Y-%m-%d %H:%M:%S] '))
+    f.write(msg)
+    f.write('\n')
+    f.close()
+
+# check file extension for validity
+def allowed_file(filename: str) -> bool:
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# check the checksum of a file
+def verify_checksum(fname: str, chksm: str) -> bool:
+    with open(fname, 'rb') as f:
+        hash_256 = hashlib.sha256(f.read()).hexdigest()
+    
+    return hash_256 == chksm
+
+
+# verify a pi's id
+def is_auth(pi_id: str) -> bool:
+
+    if pi_id == '':
+        return False
+
+    # load ids
+    ids = open(PI_ID_F, 'r')
+    data = json.load(ids)
+    ids.close()
+
+    auth = data.get(pi_id, None)
+    return auth != None
+    
+
+# generate random string
+def gen_rand_string() -> str:
+    return ''.join([choice(ascii_letters) for i in range(URL_LEN)])
+
+# add an email to a station
+def register_email(auth, email):
+    # load data
+    ids = open(PI_ID_F, 'r')
+    data = json.load(ids)
+    ids.close()
+
+    # add email
+    data[auth]['email'] = email
+    # get station num
+    num = data[auth]['station_num']
+
+    ids = open(PI_ID_F, 'w')
+    json.dump(data, ids, indent=4)
+    ids.close()
+
+    # create folder for this station
+    os.system('mkdir ' + STORAGE_FOLDER + 'station' + str(num))
+    os.system('mkdir ' + DIAGNOSTICS + 'station' + str(num))
+    
+
+#---------------------------------------------------------
+
+# make sure the necessary paths are available
+try:
+    f = open(PI_ID_F, 'r')
+    f.close()
+    os.listdir(STORAGE_FOLDER)
+    os.listdir(DIAGNOSTICS)
+except:
+    log('Error finding files and folders')
+    exit(-1)
+
+# dictionary to keep mapping of random urls to pi ids
+urls = {}
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234567'
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 128 # max file size (in KB)
+app.config['STORAGE_FOLDER'] = STORAGE_FOLDER
 
-@app.route('/')
-def hello_world():
-    sernum = request.args.get('station_serial_number')
-    if sernum == 'xyz':
-        return 'Giusto!'
-    else:
-        return str(sernum)+"|You shouldn't be here!"
+log('Server running')
 
-# route to post new data on server
-@app.route('/data', methods=['POST'])
-def download_data():
-    print(request.data)
-    print()
+#---------------------------------------------------------
 
-    return "200"
+# page for users to request their access station
+@app.route('/', methods=['GET'])
+def home():
+    return 'Temporarily out of order'
+
+# page to display access station data
+@app.route('/view/<station>')
+def view_station(station):
+    return 'Temporarily out of order'
+
+# page to register new access station to system
+# adds user email to database so they can check their station
+@app.route('/register/', methods=['POST'])
+def register():
+    # collect headers
+    auth = request.headers.get('pi_id', None)
+    email = request.headers.get('email', None)
+
+    # if id doesn't match
+    if (auth == None) or (not is_auth(auth)):
+        log('Unauthorized access, rejected')
+        return '401'
+
+    if email == None:
+        log('Required data not in request')    
+        return '412'
+
+    # add email to database
+    register_email(auth, email)
+    log('Email registered')
+
+    return '200'
+        
+
+# authentication route
+# verifies id sent by pi
+# if authenticated, returns route to upload files
+@app.route('/upload/', methods=['GET'])
+def authenticate():
+    # collect headers from request
+    auth = request.headers.get('pi_id', None)
+    num_files = request.headers.get('num_files', None)
+
+    # if request has no authentication or wrong id
+    if ((auth == None) or (not is_auth(auth))):
+        log('Unathorized access, rejected')
+        return '401'
+
+    # generate random url for data transfer
+    url = gen_rand_string()
+    # make sure rand url isn't in use
+    global urls
+    while urls.get(url, None) != None:
+        url = gen_rand_string()
+
+    log('New request from ' + auth + ', opening ' + '/upload/' + url)
+
+    # store mapping of id to random url
+    urls[url] = auth      
     
-if __name__ == "__main__":
-    #app.run(ssl_context='adhoc')
-    app.run()
+    return '301 ' + url
+
+
+# data transfer route
+# verifies again that sender has a verified id and is 
+    # transmitting to the correct channel
+@app.route('/upload/<url>', methods=['POST'])
+def get_file(url):
+    # collect headers for authentication
+    auth = request.headers.get('pi_id', None)
+
+    # verify if authorized 
+    if ((auth == None) or (urls.get(url, None) == None) or (urls[url] != auth)):
+        log('Unauthorized request, rejected')
+        return '401'
+
+    # check how many remaining files
+    num_files = request.headers.get('num_files', '1')
+    if(num_files == '1'):
+        # remove url - pi pair from existing urls
+        urls.pop(url)
+
+    # receive files
+    # check if post request has the files
+    if (('sensor_data_file' not in request.files) or 
+            ('checksum' not in request.headers) or ('pi_num' not in request.headers)):
+        log('Required data not in request')
+        return '412'
+
+    # collect files
+    datafile = request.files['sensor_data_file']
+    checksum = request.headers['checksum']
+    station_num = request.headers['pi_num']
+    
+    # check for empty fields / None
+    if ((datafile.filename == '') or (checksum == '')):
+        log('Empty file or checksum fields')
+        return '412'
+
+    # check for allowed file extensions
+    if (not allowed_file(datafile.filename)):
+        log('Unsupported file type')
+        return '415'
+    
+    # create local names for files
+    if 'diagnostic' in datafile.filename:
+        storage_folder = DIAGNOSTICS + 'station' + station_num + '/'
+    else:
+        storage_folder = app.config['STORAGE_FOLDER'] + 'station' + station_num + '/'
+    data_f_name = os.path.join(storage_folder, secure_filename(datafile.filename))
+    data_f_name_temp = os.path.join(storage_folder, secure_filename('temp_' + datafile.filename))
+
+    name = datafile.filename.split('.')[0]
+    checksum_f_name = os.path.join(storage_folder, 
+            secure_filename(name + '.sha256'))
+
+    # save data file
+    datafile.save(data_f_name_temp)
+    
+    # verify checksum
+    chksm = verify_checksum(data_f_name_temp, checksum)
+    if chksm: # successful verification
+        log('Checksum verified successfully, storing ' + data_f_name)
+        
+        # save checksume file
+        try:
+            f = open(checksum_f_name, 'w')  # overwrites if file already exists
+        except:
+            f = open(checksum_f_name, 'a')  # creates file if doesn't exist
+        f.write(checksum)
+        f.write(' ' + secure_filename(datafile.filename) + '\n')
+        f.close()
+
+        # remove temp file
+        os.system('mv ' + data_f_name_temp + ' ' + data_f_name)
+
+        # return success code
+        return '200'
+
+    else:
+        print('Wrong checksum')
+        # delete store file
+        os.remove(data_f_name_temp)
+
+        # return error
+        return '500'
