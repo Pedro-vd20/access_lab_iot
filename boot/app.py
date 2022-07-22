830789fe37199ab7e404b6bc5803fb96897bf96a
Pedro-vd20 <pedro209.vd@gmail.com>
2022-07-23 00:17:51 +0400

    Copyright (C) 2022 Francesco Paparella, Pedro Velasquez

--- boot/app.py
+++ boot/app.py
@@ -1,3 +1,26 @@
+'''
+ACCESS Lab, hereby disclaims all copyright interest in the program “ACCESS IOT 
+Stations” (which collects air and climate data) written by Francesco Paparella, 
+Pedro Velasquez.
+
+Copyright (C) 2022 Francesco Paparella, Pedro Velasquez
+
+This file is part of "ACCESS IOT Stations".
+
+"ACCESS IOT Stations" is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
+
+"ACCESS IOT Stations" is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+"ACCESS IOT Stations". If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
 from flask import Flask, request, render_template, redirect
 import os
 from modules import *
